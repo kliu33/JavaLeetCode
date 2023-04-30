d7a65d49153d0cf048f70d30cc02989efe9c54cb
Kevin Liu <30753677+kliu33@users.noreply.github.com>
2023-04-30 12:37:57 +0000

    Sync LeetCode submission - Pascal's Triangle (javascript)

---
+++ my-folder/problems/pascal's_triangle/solution.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} numRows
+ * @return {number[][]}
+ */
+var generate = function(numRows) {
+    let out = [[1]]
+    for (let i = 1; i < numRows; i++) {
+      let arr = []
+      let prev_level = out[out.length-1]
+      for (let j = 0; j <= prev_level.length; j++) {
+        if (j===0 || j === prev_level.length) {
+          arr.push(1)
+        } else {
+          arr.push(prev_level[j-1] + prev_level[j])
+        }
+      }
+      out.push(arr)
+    }
+    return out
+};
