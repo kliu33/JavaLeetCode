ff16f5e23f69e0068c7e56b8b1ba347f43069b06
Kevin Liu <30753677+kliu33@users.noreply.github.com>
2023-05-06 20:08:23 +0000

    Sync LeetCode submission - Richest Customer Wealth (javascript)

---
+++ my-folder/problems/richest_customer_wealth/solution.js
@@ -0,0 +1,7 @@
+/**
+ * @param {number[][]} accounts
+ * @return {number}
+ */
+var maximumWealth = function(accounts) {
+    return Math.max(...accounts.map(account => account.reduce((sum, a) => sum + a, 0)))
+};
