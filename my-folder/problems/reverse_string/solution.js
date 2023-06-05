0fd20e1b9751081885f69647f6f75e5c22c2fa15
Kevin Liu <30753677+kliu33@users.noreply.github.com>
2023-06-05 14:17:42 +0000

    Sync LeetCode submission - Reverse String (javascript)

--- my-folder/problems/reverse_string/solution.js
+++ my-folder/problems/reverse_string/solution.js
@@ -4,9 +4,9 @@
  */
 var reverseString = function(s) {
     let i = 0;
-    let j = s.length - 1
-    while (i < j) {
-        [s[i], s[j]] = [s[j], s[i]]
+    let j = s.length - 1;
+    while (i < j){
+        [s[i], s[j]] = [s[j],s[i]]
         i++
         j--
     }
