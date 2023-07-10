ccfa12ffd62e738697ff01f10baa6204a507e47f
Kevin Liu <30753677+kliu33@users.noreply.github.com>
2023-07-10 14:31:18 +0000

    Sync LeetCode submission - Power of Two (java)

---
+++ my-folder/problems/power_of_two/solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        if (n==0) return false;
+        int num = n;
+        while (num != 1){
+            if (num % 2 != 0){
+                return false;
+            }
+            num /= 2;
+        }
+        return true;
+    }
+}
