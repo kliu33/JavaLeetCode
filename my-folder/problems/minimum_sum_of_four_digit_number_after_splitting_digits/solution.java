8ac262baa5d48bdddb99bdad9b4345c343ea4521
Kevin Liu <30753677+kliu33@users.noreply.github.com>
2023-07-11 13:30:06 +0000

    Sync LeetCode submission - Minimum Sum of Four Digit Number After Splitting Digits (java)

---
+++ my-folder/problems/minimum_sum_of_four_digit_number_after_splitting_digits/solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int minimumSum(int num) {
+        int[] arr = new int[4];
+        for (int i = 0; i < 4; i++){
+            arr[i] = num % 10;
+            num /= 10;
+        }
+        Arrays.sort(arr);
+        int res = 0;
+        res += (arr[0] * 10) + (arr[1] * 10) + arr[2] + arr[3];
+        return res;
+    }
+}
