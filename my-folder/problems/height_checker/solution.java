93c7705b8e6e7f2c9ca7c16d0f427680fd2038ff
Kevin Liu <30753677+kliu33@users.noreply.github.com>
2023-07-11 14:42:30 +0000

    Sync LeetCode submission - Height Checker (java)

---
+++ my-folder/problems/height_checker/solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int heightChecker(int[] heights) {
+        int res = 0;
+        int[] sorted = heights.clone();
+        Arrays.sort(sorted);
+        for (int i = 0; i < heights.length; i++){
+            if (heights[i] != sorted[i]) res++;
+
+        }
+        return res;
+    }
+}
